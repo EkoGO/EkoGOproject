701343a9db424098b3afe498a5545e50c63d691c
EkoGO <seidikenov.e2005@gmail.com>
2024-10-07 21:41:54 +0500

    init

---
+++ Eko.java
@@ -0,0 +1,18 @@
+public class Eko {
+	public static void main(String[] args) {
+		System.out.println("Eko hello");
+
+		int a = 10;
+		int b = 5;
+
+		System.out.println(plus(a, b) + "");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + 1);
+		}
+	}
+
+	private static int plus(int a, int b) {
+		return a + b;
+	}
+}
